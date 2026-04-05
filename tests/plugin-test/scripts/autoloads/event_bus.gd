172462b03a9fda2403a5ae09346a85490ccb2a4e
Jan Mesarc <jame_581@windowslive.com>
2026-04-06 01:56:37 +0200

    Test projects removed

--- tests/plugin-test/scripts/autoloads/event_bus.gd
+++
@@ -1,7 +0,0 @@
-# scripts/autoloads/event_bus.gd
-extends Node
-
-signal coin_collected(value: int)
-signal player_died
-signal enemy_died(position: Vector2, coin_value: int)
-signal upgrade_purchased(upgrade_type: String, new_level: int)
